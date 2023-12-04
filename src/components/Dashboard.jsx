8cf649b5ed8daf069f6e28b305d98332627416d0
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-05 01:26:49 +0530

    dashboard created

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,30 +1,54 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import NavBar from './NavBar';
+import { useNavigate } from 'react-router-dom';
+import { protectInstance } from '../../services/instance';
 
 function Dashboard() {
+  const [totals, setTotals] = useState({ totalToday: 0, totalThisMonth: 0 });
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
-    const handleLogout = (e) => {
-        sessionStorage.removeItem('loggedInUser');
-        navigate('/');
-    };
+  const getURLCounts = async () => {
+    try {
+      const res = await protectInstance.get('/URLCounts/total');
+      setTotals(res.data);
+    } catch (error) {
+      console.error('Error fetching URL counts:', error);
+      setError('Error fetching URL counts');
+    }
+  };
+
+  useEffect(() => {
+    getURLCounts();
+  }, []);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('loggedInUser');
+    navigate('/');
+  };
 
   return (
     <div>
-    
-      <form >
-        
+      <form>
         <NavBar />
-        
+        <h1>URL Statistics</h1>
+        {error ? (
+          <div>Error: {error}</div>
+        ) : (
+          <div>
+            <h2>Total URLs created today: {totals.totalToday}</h2>
+            <h2>Total URLs created this month: {totals.totalThisMonth}</h2>
+          </div>
+        )}
       </form>
-
+      <br />
+      <br />
       <div>
-          <button onClick={handleLogout}>LOGOUT</button>
+        <button onClick={handleLogout}>LOGOUT</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Dashboard;
